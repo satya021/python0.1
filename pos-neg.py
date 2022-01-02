24e2abf77734bf9b5a88c90d3aac86bb535781bb
satyabrata <92144172+satya021@users.noreply.github.com>
2022-01-03 00:58:56 +0530

    create positive or negative

---
+++ pos-neg.py
@@ -0,0 +1,19 @@
+def check(n):
+      
+    # if the number is positive
+    if n > 0:
+        print("Positive")
+          
+    # if the number is negative
+    elif n < 0:
+        print("Negative")
+          
+    # if the number is equal to
+    # zero
+    else:
+        print("Equal to zero")
+          
+# Driver Code
+check(5)
+check(0)
+check(-5)
