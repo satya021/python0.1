8a2efe6fcb61a6f726b6ea6f76614e4af13b7c1e
satyabrata <92144172+satya021@users.noreply.github.com>
2022-01-03 00:39:50 +0530

    created relational op

---
+++ relational-op.py
@@ -0,0 +1,11 @@
+# Initializing the value of a and b  
+a = 20  
+b = 10  
+
+c = a + b  
+print ("The sum of a and b is ", c)  
+ 
+print (a > b)  
+print (a < b)  
+print (c < a)  
+print (c > b)  
